757000d37c2899699f3f233b7fc2b3e6e1c15698
Tomás Figueiredo <tomasfvfx@outlook.com>
2026-04-01 00:03:46 +0100

    refactor: simplify main function by removing unused tdlib parameters and integrating internal loading

--- client/cmd/main.go
+++ client/cmd/main.go
@@ -5,92 +5,26 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"path/filepath"
-	"strconv"
-	"syscall"
-
-	"github.com/zelenin/go-tdlib/client"
+	"telegram-proxy-client/internal"
+	"telegram-proxy-client/internal/telegram"
 )
 
 
 func main() {
-	// Import from .env file
-	var (
-		apiIdRaw = os.Getenv("API_ID")
-		apiHash  = os.Getenv("API_HASH")
-	)
-
-	apiId64, err := strconv.ParseInt(apiIdRaw, 10, 32)
+	config, err := internal.LoadEnv()
 	if err != nil {
-		log.Fatalf("strconv.ParseInt error: %s", err)
+		log.Fatalf("LoadEnv() error: %v", err)
 	}
+	
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
 
-	apiId := int32(apiId64)
-
-	// Setup tdlib parameters
-	tdlibParameters := &client.SetTdlibParametersRequest{
-		UseTestDc:           false,
-		DatabaseDirectory:   filepath.Join(".tdlib", "database"),
-		FilesDirectory:      filepath.Join(".tdlib", "files"),
-		UseFileDatabase:     true,
-		UseChatInfoDatabase: true,
-		UseMessageDatabase:  true,
-		UseSecretChats:      false,
-		ApiId:               apiId,
-		ApiHash:             apiHash,
-		SystemLanguageCode:  "en",
-		DeviceModel:         "Server",
-		SystemVersion:       "1.0.0",
-		ApplicationVersion:  "1.0.0",
-	}
-
-	authorizer := client.ClientAuthorizer(tdlibParameters)
-	go client.CliInteractor(authorizer)
-
-	_, err = client.SetLogVerbosityLevel(&client.SetLogVerbosityLevelRequest{
-		NewVerbosityLevel: 1,
-	})
+	tdlibClient, err := telegram.StartClient(config)
 	if err != nil {
-		log.Fatalf("SetLogVerbosityLevel error: %s", err)
+		log.Fatalf("StartClient() error: %v", err)
 	}
+	defer tdlibClient.Close(ctx)
 
-	tdlibClient, err := client.NewClient(authorizer)
-	if err != nil {
-		log.Fatalf("NewClient error: %s", err)
-	}
-
-	// Show tdlib and go-tdlib versions
-	versionOption, err := client.GetOption(&client.GetOptionRequest{
-		Name: "version",
-	})
-	if err != nil {
-		log.Fatalf("GetOption error: %s", err)
-	}
-
-	commitOption, err := client.GetOption(&client.GetOptionRequest{
-		Name: "commit_hash",
-	})
-	if err != nil {
-		log.Fatalf("GetOption error: %s", err)
-	}
-
-	log.Printf("TDLib version: %s (commit: %s)", versionOption.(*client.OptionValueString).Value, commitOption.(*client.OptionValueString).Value)
-
-	if commitOption.(*client.OptionValueString).Value != client.TDLIB_VERSION {
-		log.Printf("TDLib version supported by the library (%s) is not the same as TDLib version (%s)", client.TDLIB_VERSION, commitOption.(*client.OptionValueString).Value)
-	}
-
-	// Show logged user info
-	me, err := tdlibClient.GetMe(context.Background())
-	if err != nil {
-		log.Fatalf("GetMe error: %s", err)
-	}
-
-	log.Printf("Me: %s %s, Phone Number: %s", me.FirstName, me.LastName, me.PhoneNumber)
-
-	ch := make(chan os.Signal, 2)
-	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
-	<-ch
-	tdlibClient.Close(context.Background())
-	os.Exit(1)
+	log.Print("Client Started...")
+	<-ctx.Done()
 }
