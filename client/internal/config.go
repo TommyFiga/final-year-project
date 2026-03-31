895f0901dc3e50ff3a30984505013446bc2cda3c
Tomás Figueiredo <tomasfvfx@outlook.com>
2026-04-01 00:02:59 +0100

    feat: add configuration loading from environment variables

---
+++ client/internal/config.go
@@ -0,0 +1,43 @@
+package internal
+
+import (
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	ApiHash       string
+	ApiID         int32
+	BotID         int64
+	TdlibDatabase string
+	TdlibFiles    string
+}
+
+func LoadEnv() (*Config, error) {
+	var (
+		apiHash       = os.Getenv("API_HASH")
+		apiIDRaw      = os.Getenv("API_ID")
+		botIDRaw      = os.Getenv("BOT_ID")
+		tdlibDatabase = os.Getenv("TDLIB_DATABASE")
+		tdlibFiles    = os.Getenv("TDLIB_FILES")
+	)
+
+	apiID64, err := strconv.ParseInt(apiIDRaw, 10, 32)
+	if err != nil {
+		return nil, err
+	}
+	apiID := int32(apiID64)
+
+	botID, err := strconv.ParseInt(botIDRaw, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		ApiHash:       apiHash,
+		ApiID:         apiID,
+		BotID:         botID,
+		TdlibDatabase: tdlibDatabase,
+		TdlibFiles:    tdlibFiles,
+	}, nil
+}
