e13874bec72ca871a1747ebf9e49adaea5cd9761
Tomás Figueiredo <tomasfvfx@outlook.com>
2026-05-29 16:20:10 +0100

    feat: update run scripts for both client and server

--- evasion-mechanism/client/run.sh
+++ evasion-mechanism/client/run.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-SCRIPTS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "${SCRIPTS_ROOT}/.." && pwd)"
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 
 CLIENT_ROOT="${PROJECT_ROOT}/client"
 TDLIB_PATH="${CLIENT_ROOT}/tdlib"
@@ -12,7 +11,7 @@ export CGO_CFLAGS="-I${TDLIB_PATH}/include"
 export CGO_LDFLAGS="-L${TDLIB_PATH}/lib"
 export LD_LIBRARY_PATH="${TDLIB_PATH}/lib"
 
-echo "[*] Starting client..."
+echo "Starting client..."
 cd "${CLIENT_ROOT}"
 export $(grep -v '^#' .env | xargs)
 go run ./cmd
--- evasion-mechanism/server/run.sh
+++ evasion-mechanism/server/run.sh
@@ -2,12 +2,11 @@
 
 set -e
 
-SCRIPTS_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PROJECT_ROOT="$(cd "${SCRIPTS_ROOT}/.." && pwd)"
+PROJECT_ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 
 SERVER_ROOT="${PROJECT_ROOT}/server"
 
-echo "[*] Starting server..."
+echo "Starting server..."
 cd "${SERVER_ROOT}"
 export $(grep -v '^#' .env | xargs)
 go run ./cmd
